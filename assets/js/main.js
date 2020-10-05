05e06b5cb5d9d501679290bc63cbced2317682f0
sangita818 <sangeeta.tr.mdb@gmail.com>
2020-10-05 17:24:26 +0530

    upadate files

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// script for toogle
+
+// let toggle = document.querySelector(".toggle");
+// let nav = document.querySelector(".nav");
+// toggle.addEventListener('click',function(){
+//     console.log("done");
+//     nav.classList.toggle('active');
+// });
+function qwe(){
+    let nav = document.querySelector(".nav");
+    let toggle = document.querySelector(".toggle");
+    nav.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
