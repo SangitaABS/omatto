9a3c2b0d081d5e2abb214bf0ef90033b2bf9063d
sangita818 <sangeeta.tr.mdb@gmail.com>
2020-10-08 17:16:35 +0530

    image slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,33 @@
 // script for toogle
 
-// let toggle = document.querySelector(".toggle");
-// let nav = document.querySelector(".nav");
-// toggle.addEventListener('click',function(){
-//     console.log("done");
-//     nav.classList.toggle('active');
-// });
+
 function qwe(){
     let nav = document.querySelector(".nav");
     let toggle = document.querySelector(".toggle");
     nav.classList.toggle('active');
     toggle.classList.toggle('active');
 }
+
+// ==== heartIcon button Start ====
+let heartIcon = document.querySelectorAll(".heartIcon");
+for(let i of heartIcon){
+  i.addEventListener('click',function(){
+    console.log("done");
+    this.classList.toggle('active');
+  });
+}
+// ==== heartIcon button End ====
+
+// script for toggle ending here
+
+// script for slider open here
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+// script for slider closed here
