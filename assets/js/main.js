cff6b733d4955e89593db5bd7e4516307c874c7a
sangita818 <sangeeta.tr.mdb@gmail.com>
2020-10-13 17:11:54 +0530

    review and footer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
-// script for toogle
-
-
-function qwe(){
+// ===script for toogle open here ===
+function toggle__menu(){
     let nav = document.querySelector(".nav");
     let toggle = document.querySelector(".toggle");
     nav.classList.toggle('active');
     toggle.classList.toggle('active');
 }
+// ===script for toggle close here ===
 
 // ==== heartIcon button Start ====
 let heartIcon = document.querySelectorAll(".heartIcon");
@@ -18,9 +17,7 @@ for(let i of heartIcon){
 }
 // ==== heartIcon button End ====
 
-// script for toggle ending here
-
-// script for slider open here
+// === Banner Slider Script open here ===
 $('.slider').slick({
     dots: true,
     arrows: false,
@@ -30,4 +27,14 @@ $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 2000,
   });
-// script for slider closed here
+// === Banner Slider Script closed here ===
+
+// === Slider for review section open here ===
+$('.review__slider').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+// === Slider for review section open here ===
